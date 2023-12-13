897ee816de39f9d92eab300a512020ce4ebf6a05
vvk130 <vanessa.kasula@outlook.com>
2023-12-14 00:15:21 +0200

    1.6 start

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -1,22 +1,17 @@
-import Header from './components/Header'
-import Total from './components/Total'
-import Content from './components/Content'
+import { useState } from 'react'
 
-const App = (props) => {
-  const course = {
-  name: 'Half Stack application development',
-  parts: [
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14}
-  ]
-}
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-	    <Total course={course} />
+      <h2>Give feedback</h2>
+      <button>Good</button><button>Neutral</button><button>Bad</button>
+      <h2>Statistics</h2>
+      <p>Text</p>
     </div>
   )
 }
